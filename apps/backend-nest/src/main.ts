4359ccee579869fb925f8031b211f8bcf4e7ec28
ilcann <ilcan21@itu.edu.tr>
2025-09-22 19:06:19 +0300

    feat: Add Swagger documentation and update backend service configuration

--- apps/backend-nest/src/main.ts
+++ apps/backend-nest/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const serverPort = process.env.NESTJS_PORT || 5000;
+const serverUrl = process.env.NESTJS_URL || `http://localhost:5000`;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setVersion('1.0')
+    .addServer(serverUrl)
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  console.log(`Server running on port ${serverPort}`);
+  await app.listen(serverPort);
 }
-void bootstrap();
+bootstrap();
